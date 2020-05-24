46322b5d8d20b247284b26ed9025355e2c767eee
gmrammohan15 <41990680+gmrammohan15@users.noreply.github.com>
2020-05-24 21:23:09 +0530

    Update loss.py

--- S15-FinalAssignment-MaskDepth/loss.py
+++ S15-FinalAssignment-MaskDepth/loss.py
@@ -31,21 +31,13 @@ def compute_losses(inputs, outputs):
     pred = F.interpolate(
         pred, [64, 64], mode="bilinear", align_corners=False)
 
-
-    #outputs["predictive_mask"] = pred
-
-    #reprojection_losses.append(compute_reprojection_loss(pred, target_mask))
     reprojection_losses = compute_reprojection_loss(pred, target_mask)
-    #reprojection_losses = torch.cat(reprojection_losses, 1)
 
     mask = outputs["predictive_mask"]["disp", 0]
 
-    mask = F.interpolate(mask, [64, 64], mode="bilinear", align_corners=False)
-    # print(reprojection_losses.shape)
-    # print(mask.shape)         
+    mask = F.interpolate(mask, [64, 64], mode="bilinear", align_corners=False)       
     reprojection_losses *= mask
 
-    # add a loss pushing mask to 1 (using nn.BCELoss for stability)
     weighting_loss = 0.2 * nn.BCELoss()(mask, torch.ones(mask.shape).cuda())
     loss += weighting_loss.mean()
 
@@ -81,3 +73,42 @@ def alpha_prediction_loss(y_pred, y_true):
     diff = diff * mask
     num_pixels = torch.sum(mask)
     return torch.sum(torch.sqrt(torch.pow(diff, 2) + epsilon_sqr)) / (num_pixels + epsilon)
+
+class DiceCoeff(Function):
+    """Dice coeff for individual examples"""
+
+    def forward(self, input, target):
+        self.save_for_backward(input, target)
+        eps = 0.0001
+        self.inter = torch.dot(input.view(-1), target.view(-1))
+        self.union = torch.sum(input) + torch.sum(target) + eps
+
+        t = (2 * self.inter.float() + eps) / self.union.float()
+        return t
+
+    # This function has only a single output, so it gets only one gradient
+    def backward(self, grad_output):
+
+        input, target = self.saved_variables
+        grad_input = grad_target = None
+
+        if self.needs_input_grad[0]:
+            grad_input = grad_output * 2 * (target * self.union - self.inter) \
+                         / (self.union * self.union)
+        if self.needs_input_grad[1]:
+            grad_target = None
+
+        return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+    """Dice coeff for batches"""
+    if input.is_cuda:
+        s = torch.FloatTensor(1).cuda().zero_()
+    else:
+        s = torch.FloatTensor(1).zero_()
+
+    for i, c in enumerate(zip(input, target)):
+        s = s + DiceCoeff().forward(c[0], c[1])
+
+    return s / (i + 1)
